3ff52a4319d2371dd86f03c0dc02eba78d9bd6f9
hyejin0705 <hyejin3681@gmail.com>
2023-01-30 06:18:30 +0900

    [FE/FEAT] NavBar/login/signup 구현

--- front-end/swus/src/App.js
+++ front-end/swus/src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
+import NavBar from  './components/NavBar/NavBar'
+import {
+  BrowserRouter as Router,
+  Routes as Switch,
+  Route,
+  // Link
+} from "react-router-dom";
+
+import SignInSide from './pages/Accounts/Login';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <Router>
+        <NavBar />
+        <div>
+          <Switch>
+            <Route exact path="/login/" element={<SignInSide />} />
+          </Switch>
+        </div>
+      </Router>
+    </>
   );
 }
 
---
+++ front-end/swus/src/pages/Accounts/Signup.jsx
@@ -0,0 +1,75 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+
+
+export default function SignUp() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            {/* <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar> */}
+            <Typography component="h1" variant="h5">
+                Sign up
+                <Link href="#" variant="h5" style={{ textDecoration: "none", color: "black" }}>
+                    Sign in
+                </Link>
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+              아이디 (이메일)
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              비밀번호
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign Up
+              </Button>
+            </Box>
+          </Box>
+  );
+}
