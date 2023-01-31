1e8f53ec9928fdc9ff6a90a6b0b458fb26a5cede
hjlim7831 <hjlim7831@gmail.com>
2023-01-31 15:29:26 +0900

    [BE/TEST] 로그인 실패, 성공 테스트 추가

--- back-end/swus/src/test/java/com/ssaky/swus/api/service/member/MemberServiceImplTest.java
+++ back-end/swus/src/test/java/com/ssaky/swus/api/service/member/MemberServiceImplTest.java
@@ -2,7 +2,9 @@ package com.ssaky.swus.api.service.member;
 
 import com.ssaky.swus.api.request.auth.LoginReq;
 import com.ssaky.swus.api.request.auth.SignUpReq;
+import com.ssaky.swus.api.response.auth.LoginResp;
 import com.ssaky.swus.common.error.exception.InvalidValueException;
+import com.ssaky.swus.common.error.exception.custom.LoginFailException;
 import com.ssaky.swus.common.utils.TokenUtils;
 import com.ssaky.swus.db.entity.member.Member;
 import com.ssaky.swus.db.repository.member.MemberRepository;
@@ -10,11 +12,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
+
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -34,7 +38,10 @@ class MemberServiceImplTest {
     public void 회원가입() throws Exception {
 
         // given
-        SignUpReq req = new SignUpReq("helenalim1205@gmail.com", "ssafy", "상상", 2, "보광초");
+        String email = "helenalim1205@gmail.com";
+        String password = "ssafy";
+
+        SignUpReq req = SignUpReq.builder().email(email).password(password).nickname("상상").questionId(2).answer("보광초").build();
 
         // when
         int id = memberService.join(req);
@@ -48,8 +55,11 @@ class MemberServiceImplTest {
     @Test
     public void 중복회원_예외() throws Exception {
         // given
-        SignUpReq req1 = new SignUpReq("helenalim1205@gmail.com", "ssafy", "상상", 2, "보광초");
-        SignUpReq req2 = new SignUpReq("helenalim1205@gmail.com", "ssafy", "상상", 2, "보광초");
+        String email = "helenalim1205@gmail.com";
+        String password = "ssafy";
+
+        SignUpReq req1 = SignUpReq.builder().email(email).password(password).nickname("상상").questionId(2).answer("보광초").build();
+        SignUpReq req2 = SignUpReq.builder().email(email).password(password).nickname("상상").questionId(2).answer("보광초").build();
 
         // when
         int id1 = memberService.join(req1);
@@ -63,28 +73,64 @@ class MemberServiceImplTest {
 
     @Test
     @WithMockUser
-    public void 로그인() throws Exception{
+    public void 로그인_실패() throws Exception{
         // given
         String user = "user";
         String password = "password";
-        // 로그인을 위해 회원가입 해두기
-        SignUpReq signupReq = new SignUpReq(user, password, "유저", 2, "서울초");
-        int id = memberService.join(signupReq);
-        
-        LoginReq loginReq = new LoginReq("user", "password");
-        Optional<Member> member = memberService.login(loginReq);
-
-        String accessToken = TokenUtils.generateJwtToken(member.get());
-
-
 
         // when
-        String userId = TokenUtils.parseTokenToUserInfo(accessToken);
-        System.out.println(userId);
-        
-        // userId로 회원 정보 가져와서 같은 애인지 비교하기
+        LoginReq req = LoginReq.builder().email(user).password(password).build();
 
         // then
+        // 해당 유저 정보가 없으므로, LoginFailException이 떠야 함
+        assertThrows(LoginFailException.class, () -> {
+            memberService.login(req);
+        });
+    }
+
+    @Test
+    @WithAnonymousUser
+    public void 세큐리티_로그인_실패() throws Exception{
+        // given
+        String user = "user1234";
+        String password = "password1234";
+
+        // when
+        LoginReq req = LoginReq.builder().email(user).password(password).build();
+
+        // then
+        // 해당 유저 정보가 없으므로, LoginFailException이 떠야 함
+        assertThrows(LoginFailException.class, () -> {
+            memberService.login(req);
+        });
+    }
+    
+    @Test
+    @WithMockUser
+    public void 로그인_성공() throws Exception{
+
+        // given
+        String user = "user";
+        String password = "password";
+
+        // 로그인을 위해 회원가입 해두기
+        SignUpReq signupReq = SignUpReq.builder().email(user).password(password).nickname("유저").questionId(2).answer("서울초").build();
+        int id = memberService.join(signupReq);
+        
+        // 회언가입 한 애로 로그인하기
+        LoginReq loginReq = LoginReq.builder().email(user).password(password).build();
+        LoginResp resp = memberService.login(loginReq);
+        
+        // 로그인 후 받은 accessToken
+        String accessToken = resp.getAccessToken();
+
+        // when
+        
+        // 얘가 담고 있는 애가 맞는지
+        int userId = Integer.parseInt(TokenUtils.parseTokenToUserInfo(accessToken));
+
+        // then
+        assertEquals(id, userId);
 
     }
 
