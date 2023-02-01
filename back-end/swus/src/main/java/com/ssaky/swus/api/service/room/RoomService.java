ecc44b2a4ad30de5ad956e8544ee5211bd03d2c7
노정환 <zdsa43@gmail.com>
2023-02-01 23:18:19 +0900

    [BE/FEAT] 공용 열람실 입장 기능 구현
    Participant 엔티티 생성
    정원 초과시 예외처리,
    정원초과 테스트코드 작성

--- back-end/swus/src/main/java/com/ssaky/swus/api/service/room/RoomService.java
+++ back-end/swus/src/main/java/com/ssaky/swus/api/service/room/RoomService.java
@@ -1,13 +1,20 @@
 package com.ssaky.swus.api.service.room;
 
+import com.ssaky.swus.common.error.exception.custom.OverCapacityException;
+import com.ssaky.swus.db.entity.Room.PublicParticipant;
 import com.ssaky.swus.db.entity.Room.PublicRoom;
+import com.ssaky.swus.db.entity.member.Member;
+import com.ssaky.swus.db.repository.member.MemberRepository;
+import com.ssaky.swus.db.repository.room.ParticipantRepository;
 import com.ssaky.swus.db.repository.room.RoomRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
@@ -16,13 +23,15 @@ import java.util.List;
 public class RoomService {
 
     private final RoomRepository roomRepository;
+    private final MemberRepository memberRepository;
+    private final ParticipantRepository participantRepository;
+    private final int LIMIT = 9; //공용 열람실의 최대 인원수
 
     @Transactional
     public int createPublic(String type) {
         PublicRoom publicRoom = new PublicRoom();
         publicRoom.setType(type);
-        int room_id = roomRepository.createPublic(publicRoom);
-        return room_id;
+        return roomRepository.createPublic(publicRoom);
     }
 
     public List<PublicRoom> findPublics() {
@@ -31,12 +40,23 @@ public class RoomService {
 
     @Transactional
     public void enterPublic(int room_id, int user_id){
+        PublicRoom room = roomRepository.findPublicOne(room_id);
+        //Optional<T>를 쓰면 T가 null인 경우를 방지해줌
+        Optional<Member> member = memberRepository.findOne(user_id);
 
         //기능1. 방 정원을 넘는지 검사
+        if (room.getCount() >= LIMIT) {
+            throw new OverCapacityException(LIMIT,room.getCount());
+        }
 
         //기능2. room_id의 참가자 1 증가시키기
         roomRepository.updateCount(room_id, 1);
 
         //기능3. user를 Paricipant에 insert해주기
+        PublicParticipant participant = PublicParticipant.builder()
+                .room(room)
+                .member(member.get())
+                .joined_at(LocalDateTime.now()).build();
+        participantRepository.joinPublic(participant);
     }
 }
