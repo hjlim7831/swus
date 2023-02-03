bbeb4a0ba3ff2bae03c03d6b5e36e6a7556be2f8
hyejin0705 <hyejin3681@gmail.com>
2023-02-03 17:38:02 +0900

    [FE/REFACTOR] SideBar 재사용성을 위해 공통 폴더에 업로드

--- front-end/swus/src/index.js
+++ front-end/swus/src/index.js
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
-import store from "./store/Store.jsx";
-
+import store from "./store/store.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
