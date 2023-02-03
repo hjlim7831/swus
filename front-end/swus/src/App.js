00a6b6ea1602d2c57156cc89323cf721372153a7
xguu <xguu9604@gmail.com>
2023-02-03 17:26:35 +0900

    [FE/REFACTOR] 컴포넌트 별 라우터 주소 지정

--- front-end/swus/src/App.js
+++ front-end/swus/src/App.js
@@ -4,16 +4,54 @@ import {
   Route,
   // Link
 } from "react-router-dom";
-import NavBar from "./components/NavBar/NavBar";
-import SignInSide from "./pages/Accounts/LogIn";
-import SignUpSide from "./pages/Accounts/SignUp";
-import FindPassword from "./pages/Accounts/FindPassword";
-import BasicModalDialog from "./pages/Accounts/UserModal";
-import Lounge from "./pages/Lounge/SideBar";
-import GroupPage from "./pages/GroupPage/GroupPage";
+// import NavBar from "./components/NavBar/NavBar";
+// import SignInSide from "./pages/Accounts/LogIn";
+// import SignUpSide from "./pages/Accounts/SignUp";
+// import FindPassword from "./pages/Accounts/FindPassword";
+// import BasicModalDialog from "./pages/Accounts/UserModal";
+// import Lounge from "./pages/Lounge/SideBar";
+// import GroupPage from "./pages/GroupPage/GroupPage";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          {/* landingPage */}
+          <Route exact path="/" />
+          {/* 공용 열람실 관련 주소 */}
+          <Route exact path="/studyroom">
+            <Route exact path="/" />
+            <Route exact path="/:sessionName" />
+          </Route>
+          {/* 마이 페이지 관련 주소 */}
+          <Route exact path="/mypage">
+            <Route exact path="/profile/:userId" />
+            <Route exact path="/group/:groupId" />
+            <Route exact path="/group/:groupId/update" />
+            <Route exact path="/myreport/:userId" />
+          </Route>
+          {/* 그룹 페이지(게시판) 관련 주소 */}
+          <Route exact path="/group">
+            <Route exact path="/mystudy/:userId" />
+            <Route exact path="/board" />
+            <Route exact path="board/:boardId" />
+            <Route exact path="/board/:boardId/update" />
+            <Route exact path="/board/create" />
+            <Route exact path="/studyroom/:sessionName" />
+          </Route>
+          {/* 휴게실 */}
+          <Route exact path="/lounge" />
+          {/* 회원 정보 관련 주소 */}
+          <Route exact path="/account">
+            <Route exact path="/login" />
+            <Route exact path="/signup" />
+            <Route exact path="/findpassword" />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
 
 export default App;
