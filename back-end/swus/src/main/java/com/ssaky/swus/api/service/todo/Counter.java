0b5bd7db55d3eb3086d141642f08cc9abb3a3870
hjlim <hjlim7831@gmail.com>
2023-02-04 21:31:42 +0900

    [BE/TEST] JandiTodo 관련 테스트 코드 추가
    Schedule TEST 코드 정상 작동 확인 (Counter 로 확인)
    TodoServiceTest - 어제 날짜 가져오는 함수 테스트 추가
    TodoDailyUpdateServiceTest - todoDone 정기 삭제 테스트 완료, 조회 테스트 코드 작성중

--- back-end/swus/src/main/java/com/ssaky/swus/api/service/todo/Counter.java
+++ back-end/swus/src/main/java/com/ssaky/swus/api/service/todo/Counter.java
@@ -5,11 +5,13 @@ import org.springframework.stereotype.Component;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
+// Scheduler가 돌아가는지 확인하기 위한 테스트 Component
+// @Scheduled 주석을 풀고 테스트 코드를 돌리면 됨
 @Component
 public class Counter {
     private AtomicInteger count = new AtomicInteger(0);
 
-    @Scheduled(fixedDelay = 5)
+//    @Scheduled(fixedDelay = 5)
     public void scheduled(){
         this.count.incrementAndGet();
     }
--- back-end/swus/src/test/java/com/ssaky/swus/api/service/todo/TodoDailyUpdateServiceTest.java
+++ back-end/swus/src/test/java/com/ssaky/swus/api/service/todo/TodoDailyUpdateServiceTest.java
@@ -5,7 +5,7 @@ import com.ssaky.swus.api.request.todo.TodoCreateReq;
 import com.ssaky.swus.api.request.todo.TodoUpdateReq;
 import com.ssaky.swus.api.response.todo.TodoGetResp;
 import com.ssaky.swus.api.service.member.MemberService;
-import com.ssaky.swus.db.entity.todo.TodoPrivate;
+import com.ssaky.swus.db.repository.todo.MemberTodoCount;
 import com.ssaky.swus.db.repository.todo.TodoRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,17 +17,15 @@ import org.springframework.test.util.ReflectionTestUtils;
 
 import javax.transaction.Transactional;
 
-import java.sql.Date;
 import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-@Transactional
-class TodoServiceTest {
+//@Transactional
+public class TodoDailyUpdateServiceTest {
 
     @Autowired TodoRepository todoRepository;
     @Autowired TodoService todoService;
@@ -52,59 +50,8 @@ class TodoServiceTest {
                 .nickname("싸피").questionId(2).answer("싸피초").build();
 
         memberId2 = memberService.join(signUpReq2);
-    }
 
-    @Test
-    public void todo등록(){
-        // given
-        TodoCreateReq todoCreateReq = new TodoCreateReq("알고리즘 한 문제 풀기");
-
-        // when
-        int num = todoService.save(todoCreateReq, memberId);
-
-        // then
-        TodoPrivate res = todoRepository.findOne(num, memberId).get();
-
-        assertEquals(num, res.getNum());
-    }
-
-    @Test
-    public void todo수정(){
-        // given
-        TodoCreateReq todoCreateReq = new TodoCreateReq("알고리즘 한 문제 풀기");
-        int num = todoService.save(todoCreateReq, memberId);
-
-        // when
-        TodoUpdateReq todoUpdateReq = TodoUpdateReq.builder()
-                .content("알고리즘 두 문제 풀기")
-                .todoDone("Y").build();
-        todoService.update(num, todoUpdateReq, memberId);
-
-        // then
-        TodoPrivate res = todoRepository.findOne(num, memberId).get();
-
-        assertEquals(todoUpdateReq.getTodoDone(), res.getTodoDone());
-        assertEquals(todoUpdateReq.getContent(), res.getContent());
-    }
-
-    @Test
-    public void todo삭제(){
-        // given
-        TodoCreateReq todoCreateReq = new TodoCreateReq("알고리즘 한 문제 풀기");
-        int num = todoService.save(todoCreateReq, memberId);
-
-        // when
-        todoService.delete(num, memberId);
-
-        // then
-        Optional<TodoPrivate> res = todoRepository.findOne(num, memberId);
-
-        assert(res.isEmpty());
-    }
-
-    @Test
-    public void todo조회(){
-        // given
+        // 투두 내용
         String content1 = "알고리즘 한 문제 풀기";
         String content2 = "JPA 복습하기";
         String content3 = "테스트 코드 작성하기";
@@ -113,41 +60,56 @@ class TodoServiceTest {
         TodoCreateReq todoCreateReq3 = new TodoCreateReq(content3);
 
         String content4 = "인강 1개 듣기";
+        String content5 = "물마시기";
         TodoCreateReq todoCreateReq4 = new TodoCreateReq(content4);
+        TodoCreateReq todoCreateReq5 = new TodoCreateReq(content5);
 
-        // when
+        // 투두 기록
         int num1 = todoService.save(todoCreateReq1, memberId);
         int num2 = todoService.save(todoCreateReq2, memberId);
         int num3 = todoService.save(todoCreateReq3, memberId);
         int num4 = todoService.save(todoCreateReq4, memberId2);
+        int num5 = todoService.save(todoCreateReq5, memberId2);
 
-        TodoUpdateReq todoUpdateReq = TodoUpdateReq.builder()
+        // 완료한 Todo들 update 처리
+        TodoUpdateReq todoUpdateReq1 = TodoUpdateReq.builder()
                 .content(content1).todoDone("Y").build();
-        todoService.update(num1, todoUpdateReq, memberId);
+        TodoUpdateReq todoUpdateReq2 = TodoUpdateReq.builder()
+                .content(content2).todoDone("Y").build();
+        TodoUpdateReq todoUpdateReq4 = TodoUpdateReq.builder()
+                .content(content4).todoDone("Y").build();
 
-        // then
-        List<TodoGetResp> list = todoService.getList(memberId);
-        System.out.println(list);
-        assertEquals(3, list.size());
-        assertEquals(content1, list.get(0).getContent());
-        assertEquals(content2, list.get(1).getContent());
-        assertEquals(content3, list.get(2).getContent());
-        assertEquals(num1, list.get(0).getNum());
-        assertEquals(num2, list.get(1).getNum());
-        assertEquals(num3, list.get(2).getNum());
-        assertEquals("Y", list.get(0).getTodoDone());
-
-        List<TodoGetResp> list2 = todoService.getList(memberId2);
-        assertEquals(1, list2.size());
-        assertEquals(content4, list2.get(0).getContent());
+        todoService.update(num1, todoUpdateReq1, memberId);
+        todoService.update(num2, todoUpdateReq2, memberId);
+        todoService.update(num4, todoUpdateReq4, memberId2);
 
     }
 
     @Test
-    public void 어제날짜_가져오기(){
-        // when
-        Date yesterday = ReflectionTestUtils.invokeMethod(todoService, "getYesterday");
+    public void todoDone이_Y인_경우_모두_지우기(){
+        todoRepository.deleteAllDoneInQuery();
+        List<TodoGetResp> list1 = todoRepository.findList(memberId);
+        List<TodoGetResp> list2 = todoRepository.findList(memberId2);
+        assertEquals(1, list1.size());
+        assertEquals(1, list2.size());
+
     }
+
+    @Test
+    public void 모든_사용자의_TODO_잔디기록하기(){
+        // given
+        List<MemberTodoCount> todoCountList = todoRepository.findTodoCountGroupByMember();
+
+        // when
+        ReflectionTestUtils.invokeMethod(todoService, "saveAllDailyTodoCount", todoCountList);
+
+        // then
+
+
+
+    }
+
+
 
 
 }
