30778e38cd2d1762106b5d6886914ad8223c4a93
minn12 <tlsalsgpp12@gmail.com>
2023-02-06 16:13:21 +0900

    [BE/FEAT] 모집게시글(board) update & delete

--- back-end/swus/src/main/java/com/ssaky/swus/api/controller/group/BoardController.java
+++ back-end/swus/src/main/java/com/ssaky/swus/api/controller/group/BoardController.java
@@ -1,6 +1,9 @@
 package com.ssaky.swus.api.controller.group;
 
+import com.ssaky.swus.api.request.group.UpdateBoardReq;
 import com.ssaky.swus.api.request.group.WriteBoardReq;
+import com.ssaky.swus.api.response.group.BoardGetResp;
+import com.ssaky.swus.api.response.group.BoardListResp;
 import com.ssaky.swus.api.service.group.BoardService;
 import com.ssaky.swus.common.utils.TokenUtils;
 import io.jsonwebtoken.Claims;
@@ -8,12 +11,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -37,6 +39,47 @@ public class BoardController {
         return ResponseEntity.ok(resultMap);
     }
 
+    @GetMapping("/{board_id}")
+    public ResponseEntity<?> getPost(Authentication authentication, @PathVariable("board_id") int boardId) {
 
+        Claims claims = (Claims) authentication.getPrincipal();
+        int memberId = TokenUtils.getmemberIdFromToken(claims);
 
+        BoardGetResp boardGetResp = boardService.getDetailBoard(boardId);
+        return ResponseEntity.ok(boardGetResp);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getPostList(Authentication authentication) {
+        Claims claims = (Claims) authentication.getPrincipal();
+        int memberId = TokenUtils.getmemberIdFromToken(claims);
+
+        List<BoardListResp> boardList = boardService.getBoardList();
+        return ResponseEntity.ok(boardList);
+    }
+
+    @PatchMapping("/{board_id}")
+    public ResponseEntity<?> updatePost(Authentication authentication, @PathVariable("board_id") Integer boardId, @RequestBody @Valid UpdateBoardReq updateBoardReq) {
+        Claims claims = (Claims) authentication.getPrincipal();
+        int memberId = TokenUtils.getmemberIdFromToken(claims);
+
+        boardService.updateBoard(boardId, updateBoardReq);
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("msg", "success_update_board");
+        return ResponseEntity.ok(resultMap);
+    }
+
+    @DeleteMapping("/{board_id}")
+    public ResponseEntity<?> deletePost(Authentication authentication, @PathVariable("board_id") int boardId) {
+        Claims claims = (Claims) authentication.getPrincipal();
+        int memberId = TokenUtils.getmemberIdFromToken(claims);
+
+        boardService.deleteBoard(boardId);
+        Map<String, Object> resultMap = new HashMap<>();
+        StringBuilder sb = new StringBuilder();
+        sb.append("success_delete_board_id_");
+        sb.append(boardId);
+        resultMap.put("msg", sb);
+        return ResponseEntity.ok(resultMap);
+    }
 }
--- back-end/swus/src/main/java/com/ssaky/swus/api/service/group/BoardService.java
+++ back-end/swus/src/main/java/com/ssaky/swus/api/service/group/BoardService.java
@@ -1,13 +1,20 @@
 package com.ssaky.swus.api.service.group;
 
+import com.ssaky.swus.api.request.group.UpdateBoardReq;
+import com.ssaky.swus.api.request.group.UpdaterBoardReq;
 import com.ssaky.swus.api.request.group.WriteBoardReq;
+import com.ssaky.swus.api.response.group.BoardGetResp;
+import com.ssaky.swus.api.response.group.BoardListResp;
 import com.ssaky.swus.db.entity.group.Board;
 import com.ssaky.swus.db.repository.group.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -18,7 +25,6 @@ public class BoardService {
 
     @Transactional
     public int writeBoard(int memberId, WriteBoardReq req) {
-        System.out.println(req);
         Board board = Board.builder()
                 .memberId(memberId)
                 .title(req.getTitle())
@@ -26,5 +32,61 @@ public class BoardService {
                 .number(req.getBoardNumber())
                 .build();
         return boardRepository.save(board).getBoardId();
+    }
+
+    @Transactional
+    public BoardGetResp getDetailBoard(int boardId) {
+        Board board = boardRepository.findById(boardId)
+                .orElseThrow(IllegalArgumentException::new);    // Id값에 해당하는 게시글이 없을때
+
+        System.out.println(board);
+        return BoardGetResp.builder()
+                .title(board.getTitle())
+                .content(board.getContent())
+                .views(board.getViews())
+                .writeAt(board.getWriteAt())
+                .boardNumber(board.getNumber())
+                .build();
+    }
+
+    @Transactional
+    public List<BoardListResp> getBoardList() {
+        List<Board> boards = boardRepository.findAll();
+        List<BoardListResp> boardList = new ArrayList<>();
+
+        for (Board board : boards) {
+            BoardListResp resp = BoardListResp.builder()
+                    .board_id(board.getBoardId())
+                    .title(board.getTitle())
+                    .writeAt(board.getWriteAt())
+                    .views(board.getViews())
+                    .build();
+
+            boardList.add(resp);
+        }
+        return boardList;
+    }
+
+    @Transactional
+    public void updateBoard(int boardId, UpdateBoardReq updateBoardReq) {
+        Board board = boardRepository.findById(boardId)
+                .orElseThrow(RuntimeException::new);
+
+        UpdaterBoardReq.UpdaterBoardReqBuilder updaterBoardReqBuilder = board.toUpdater();
+
+        UpdaterBoardReq updaterBoardReq = updaterBoardReqBuilder
+                .title(updateBoardReq.getTitle())
+                .content(updateBoardReq.getContent())
+                .boardNumber(updateBoardReq.getBoardNumber())
+                .build();
+
+        board.update(updaterBoardReq);
+    }
+
+    @Transactional
+    public void deleteBoard(int boardId) {
+        Board board = boardRepository.findById(boardId)
+                .orElseThrow(RuntimeException::new);
+        boardRepository.delete(board);
     }
 }
