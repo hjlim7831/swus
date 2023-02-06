62d6af98cd0c21789e2057de4dddadfdea275709
hjlim7831 <hjlim7831@gmail.com>
2023-02-06 17:46:27 +0900

    [BE/TEST] 회원탈퇴 시 연관 테이블 삭제 추가 (진행중)
    - 투두, 잔디, 공부 테이블 테스트 추가
    - 공공열람실 (진행중)

--- back-end/swus/src/test/java/com/ssaky/swus/api/service/member/MemberServiceTest.java
+++ back-end/swus/src/test/java/com/ssaky/swus/api/service/member/MemberServiceTest.java
@@ -1,29 +1,33 @@
 package com.ssaky.swus.api.service.member;
 
-import com.ssaky.swus.api.request.auth.CheckPwdReq;
-import com.ssaky.swus.api.request.auth.LoginReq;
 import com.ssaky.swus.api.request.auth.SignUpReq;
 import com.ssaky.swus.api.request.member.MemberUpdateReq;
-import com.ssaky.swus.api.response.auth.LoginResp;
+import com.ssaky.swus.api.request.todo.TodoCreateReq;
+import com.ssaky.swus.api.request.todo.TodoUpdateReq;
 import com.ssaky.swus.api.response.member.MemberInfoGetResp;
+import com.ssaky.swus.api.response.todo.TodoGetResp;
+import com.ssaky.swus.api.response.todo.TodoJandiResp;
+import com.ssaky.swus.api.service.study.StudyService;
+import com.ssaky.swus.api.service.todo.TodoService;
 import com.ssaky.swus.common.error.exception.InvalidValueException;
-import com.ssaky.swus.common.error.exception.custom.LoginFailException;
-import com.ssaky.swus.common.error.exception.custom.UncorrectAnswerException;
-import com.ssaky.swus.common.utils.TokenUtils;
 import com.ssaky.swus.db.entity.member.Member;
 import com.ssaky.swus.db.repository.member.MemberRepository;
-import io.jsonwebtoken.Claims;
+import com.ssaky.swus.db.repository.study.StudyRepository;
+import com.ssaky.swus.db.repository.todo.JandiTodoRepository;
+import com.ssaky.swus.db.repository.todo.TodoRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithAnonymousUser;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -32,12 +36,15 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 // SpringExtension을 사용하게 되면 Spring TestContext Framework와 Junit5와 통합하여 사용하게 됨
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-@Transactional
 class MemberServiceTest {
 
-    @Autowired
-    MemberRepository memberRepository;
+    @Autowired MemberRepository memberRepository;
+    @Autowired TodoRepository todoRepository;
+    @Autowired StudyRepository studyRepository;
+
     @Autowired MemberService memberService;
+    @Autowired TodoService todoService;
+    @Autowired StudyService studyService;
 
     static int memberId;
     static int memberId2;
@@ -58,6 +65,11 @@ class MemberServiceTest {
                 .nickname("싸피").questionId(2).answer("싸피초").build();
 
         memberId2 = memberService.join(signUpReq2);
+    }
+
+    @AfterEach
+    void afterEach() {
+        memberRepository.deleteAll();
     }
 
     @Test
@@ -98,15 +110,69 @@ class MemberServiceTest {
     }
 
     @Test
-    public void 회원_탈퇴() {
+    public void 회원_탈퇴_작동확인() {
         memberService.delete(memberId);
-//        memberService.deleteById(memberId);
-        // TODO: 다른 테이블도 살아있는지 확인하기
         assertEquals(1, memberRepository.count());
+    }
 
+    @Test
+    public void 회원_탈퇴후_투두_테이블_확인(){
+        // 투두 생성해두기
+        TodoCreateReq req = TodoCreateReq.builder().content("JPA 공부하기").build();
+        todoService.save(req, memberId);
+        
+        // 탈퇴 전 테이블 확인
+        List<TodoGetResp> listBefore = todoService.getList(memberId);
+        assertEquals(1, listBefore.size());
+        
+        // 탈퇴
+        memberService.delete(memberId);
+        
+        // 탈퇴 후 테이블 확인
+        List<TodoGetResp> listAfter = todoService.getList(memberId);
+        assertEquals(0, listAfter.size());
+    }
+
+    @Test
+    public void 회원_탈퇴후_잔디_테이블_확인(){
+        // 투두 생성해두기
+        String content = "JPA 공부하기";
+        TodoCreateReq req = TodoCreateReq.builder().content(content).build();
+        int num = todoService.save(req, memberId);
+        
+        // 투두 Y로 업데이트
+        TodoUpdateReq updateReq = TodoUpdateReq.builder().content(content).todoDone("Y").build();
+        todoService.update(num, updateReq, memberId);
+
+        // 잔디 생성해두기
+        ReflectionTestUtils.invokeMethod(todoService, "dailyUpdate");
+
+        TodoJandiResp jandiRecordsBefore = todoService.getJandiRecords(memberId);
+        assertEquals(1, jandiRecordsBefore.getTodoRecords().size());
+        
+        // 회원 탈퇴하기
+        memberService.delete(memberId);
+
+        // 탈퇴 후 jandi 테이블 확인하기
+        TodoJandiResp jandiRecordsAfter = todoService.getJandiRecords(memberId);
+        assertEquals(0, jandiRecordsAfter.getTodoRecords().size());
 
     }
 
+    @Test
+    public void 회원_탈퇴후_공부_테이블_확인(){
+        // 탈퇴 전 테이블 확인
+        assertEquals(1, studyRepository.countByMemberId(memberId));
+        
+        // 회원 탈퇴
+        memberService.delete(memberId);
+        
+        // 탈퇴 후 테이블 확인
+        assertEquals(0, studyRepository.countByMemberId(memberId));
+    }
 
-
+    @Test
+    public void 회원_탈퇴후_공공열람실_참여자_테이블_확인(){
+        // TODO: 열람실 참여자로 생성해두기
+    }
 }
