cdbf38780298bb8bb0ec2b973743f335de5ffdc2
zdsa43 <zdsa43@gmail.com>
2023-02-07 13:31:46 +0900

    [BE/FEAT] 공용열람실 입장시 JWT에서 값을 받아오도록 수정

--- back-end/swus/src/main/java/com/ssaky/swus/api/controller/room/RoomController.java
+++ back-end/swus/src/main/java/com/ssaky/swus/api/controller/room/RoomController.java
@@ -3,10 +3,13 @@ package com.ssaky.swus.api.controller.room;
 import com.ssaky.swus.api.request.room.PublicCreateReq;
 import com.ssaky.swus.api.request.room.PublicExitReq;
 import com.ssaky.swus.api.service.room.RoomService;
+import com.ssaky.swus.common.utils.TokenUtils;
 import com.ssaky.swus.db.entity.Room.PublicRoom;
+import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -42,17 +45,21 @@ public class RoomController {
     }
 
     @PostMapping("/studyrooms/{room_id}")
-    public ResponseEntity<?> enterPublic (@RequestBody Map<String, Object> userIdMap, @PathVariable int room_id){
-        int user_id = (Integer) userIdMap.get("user_id");
-        roomService.enterPublic(room_id,user_id);
+    public ResponseEntity<?> enterPublic (Authentication authentication, @PathVariable int room_id){
+        Claims claims = (Claims) authentication.getPrincipal();
+        int userId = TokenUtils.getmemberIdFromToken(claims);
+        roomService.enterPublic(room_id,userId);
         Map<String, Object> resultMap = new HashMap<>();
         resultMap.put("msg", "success_enter_studyroom");
         return ResponseEntity.ok(resultMap);
     }
 
     @PostMapping("/studyrooms/exit")
-    public ResponseEntity<?> exitPublic(@RequestBody PublicExitReq publicExitReq) {
-        log.debug("member_id/room_id : "+publicExitReq.getMemberId()+"/"+publicExitReq.getRoomId());
+    public ResponseEntity<?> exitPublic(Authentication authentication, @RequestBody PublicExitReq publicExitReq) {
+        Claims claims = (Claims) authentication.getPrincipal();
+        int userId = TokenUtils.getmemberIdFromToken(claims);
+        publicExitReq.setMemberId(userId);
+        log.debug("member_id/room_id : "+userId+"/"+publicExitReq.getRoomId());
         roomService.exitPublic(publicExitReq);
         Map<String, Object> resultMap = new HashMap<>();
         resultMap.put("msg", "success_exit_studyroom");
