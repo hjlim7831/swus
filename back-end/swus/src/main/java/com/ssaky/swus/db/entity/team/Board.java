3b1cc2715a96ae302f7e498a6b53cdfcb5df05f7
tlsalsgpp12@gmail.com <tlsalsgpp12@gmail.com>
2023-02-09 09:14:05 +0900

    [BE/FEAT] Board 엔티티 수정

--- back-end/swus/src/main/java/com/ssaky/swus/db/entity/team/Board.java
+++ back-end/swus/src/main/java/com/ssaky/swus/db/entity/team/Board.java
@@ -41,9 +41,11 @@ public class Board extends BaseDateEntity {
     private int number;     // 최종모집인원
 
     @Builder
-    public Board(int memberId, String title, String content, int number) {
+    public Board(int memberId, int teamId, String title, String content, int number) {
         Member member = Member.builder().id(memberId).build();
         this.member = member;
+        Team team = Team.builder().teamId(teamId).build();
+        this.team = team;
         this.title = title;
         this.content = content;
         this.views = 0;
