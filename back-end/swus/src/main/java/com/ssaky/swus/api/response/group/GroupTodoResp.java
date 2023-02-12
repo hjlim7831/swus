f926c30c02596c809b9973c4ee49bce0f172268c
hjlim <hjlim7831@gmail.com>
2023-02-12 10:43:16 +0900

    [BE/FIX] groupTodoUpdate 오류 수정
    round와 teamId가 save 시 반대로 저장되는 오류 발견
    todoGroupId 에서 두 개의 변수를 받아오는 생성자의 변수 순서를 바꾸어 오류 해결

--- back-end/swus/src/main/java/com/ssaky/swus/api/response/group/GroupTodoResp.java
+++ back-end/swus/src/main/java/com/ssaky/swus/api/response/group/GroupTodoResp.java
@@ -2,8 +2,13 @@ package com.ssaky.swus.api.response.group;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import lombok.Value;
 
+@Getter
+@ToString
 @Value
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class GroupTodoResp {
--- back-end/swus/src/main/java/com/ssaky/swus/db/entity/report/TodoGroupId.java
+++ back-end/swus/src/main/java/com/ssaky/swus/db/entity/report/TodoGroupId.java
@@ -14,12 +14,16 @@ import java.io.Serializable;
 @Getter
 @NoArgsConstructor
 @Builder
+@ToString
 public class TodoGroupId implements Serializable {
     // 복합키에는 Serializable 을 implement 해야 함
+    
+    // save를 할 때, type이 같으면 그냥 알파벳 순서대로 집어넣는 것 같기도 하다
+    // 이부분은 JPA Repository에서 복합키인 객체를 어떻게 저장하는지를 확인해야 할 것 같다
 
-    public TodoGroupId(int teamId, int round) {
-        this.teamId = teamId;
+    public TodoGroupId(int round, int teamId) {
         this.round = round;
+        this.teamId = teamId;
     }
 
     @EqualsAndHashCode.Include
