0ec5950593bcc6777145546dfa8195763bc88911
hjlim <hjlim7831@gmail.com>
2023-02-13 09:13:43 +0900

    [FE/FEAT] 사진 추가, 캐러셀 일부 구성

--- front-end/swus/src/pages/LandingPage/LandingPage.jsx
+++ front-end/swus/src/pages/LandingPage/LandingPage.jsx
@@ -1,29 +1,34 @@
 import React from 'react'
 import Carousel from 'react-material-ui-carousel'
-import { Paper, Button } from '@mui/material'
+import { Button, Card, Typography, CardActions } from '@mui/material'
+
+import imgClock from '../../image/landingPage/watch.jpg'
+import imgBench from '../../image/landingPage/benches.jpg'
+import imgGroup from '../../image/landingPage/group.jpg'
+import imgSched from '../../image/landingPage/scheduler.jpg'
 
 function Example(props)
 {
     var items = [
         {
-            name: "바로 로그인",
-            description: "Probably the most random thing you have ever seen!"
-        },
-        {
             name: "공용 열람실",
-            description: "Probably the most random thing you have ever seen!"
-        },
-        {
-            name: "마이 페이지",
-            description: "Probably the most random thing you have ever seen!"
+            description: "Probably the most random thing you have ever seen!",
+            img:imgClock
         },
         {
             name: "그룹 스터디",
-            description: "Probably the most random thing you have ever seen!"
+            description: "Probably the most random thing you have ever seen!",
+            img:imgGroup
+        },
+        {
+            name: "마이 페이지",
+            description: "Probably the most random thing you have ever seen!",
+            img:imgSched
         },
         {
             name: "휴게실",
-            description: "Hello World!"
+            description: "Hello World!",
+            img:imgBench
         }
     ]
 
@@ -38,23 +43,38 @@ function Example(props)
 
 function Item(props)
 {
-    return (
-        <Paper>
-            <h2>{props.item.name}</h2>
-            <p>{props.item.description}</p>
 
-            <Button className="CheckButton">
-                LOG IN!
-            </Button>
-        </Paper>
-    )
+    const styles = {
+        categoryCard: {
+            backgroundImage: `linear-gradient( rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3) ), url(${props.item.img})`,
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            height: 600,
+            width: 1000
+        }
+    }
+
+    return (
+        <Card style={styles.categoryCard}>
+        <Typography mt={3} variant="h4" style={{color:"white"}}>
+            {props.item.name}
+        </Typography>
+        <Typography variant="body2" style={{color:"white"}}>
+        {props.item.description}
+            </Typography>
+        <div>
+        <Button variant="outlined" style={{color:"white", borderColor:"white"}}>
+            LOG IN!
+        </Button>
+        </div>
+    </Card>
+)
 }
 
 
 function LandingPage() {
   return (
     <>
-        <div>LandingPage</div>
         <Example></Example>
     </>
   )
